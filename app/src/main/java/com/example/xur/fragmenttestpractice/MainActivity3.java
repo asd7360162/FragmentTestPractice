4cc9089e3f1fbdd0b4e9e3c7c302f1a0dc2a971c
xur <645531705@qq.com>
2016-10-19 10:08:31 +0800

    201610191008

--- app/src/main/java/com/example/xur/fragmenttestpractice/MainActivity3.java
+++ app/src/main/java/com/example/xur/fragmenttestpractice/MainActivity3.java
@@ -10,6 +10,6 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_main3);//
     }
 }
